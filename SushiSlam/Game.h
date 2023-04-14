05dc4bf8892a1591accc5ed2a8923a1a3374d1e9
Xiaojian ZHENG <zhengjian.au@gmail.com>
2023-04-14 18:33:19 +0930

    Add game class skeleton

--- SushiSlam/Game.h
+++ SushiSlam/Game.h
@@ -1,1 +1,21 @@
-#pragma once
+#include <iostream>
+#include <vector>
+#include <string>
+#include "Card.h"
+#include "Player.h"
+#include "Deck.h"
+
+using namespace std;
+
+class Game {
+public:
+    Game();
+    void start();
+
+private:
+    vector<Player> m_players;
+    card_collection m_deck;
+    int round_count;
+
+    void print_round();
+};
